d6371ce6a2714f36d94afcdb2110e0de3148822d
Guangxing Zhu <gzhu0009@student.monash.edu>
2023-04-25 01:03:17 +1000

    Third commit of 9MM code including all the classes related to Action class.

---
+++ Nine_Mans_Morris_Code/UndoAction.java
@@ -0,0 +1,13 @@
+package Nine_Mans_Morris_Code;
+
+public class UndoAction extends Action {
+
+    public Action getPreviousMove(){
+        return null;
+    }
+
+    @Override
+    public Player execute(Player player, Board board) {
+        return null;
+    }
+}
